975caf766ee44ed33439cb3124f5d5e686a3a570
Neel <neeljverma.1@gmail.com>
2018-05-10 20:08:48 -0400

    Optimized longest substring problem.

--- Longest_Substring_With_K_Distinct_Characters/main.py
+++ Longest_Substring_With_K_Distinct_Characters/main.py
@@ -6,45 +6,32 @@ For example, given s = "abcba" and k = 2, the longest substring with k distinct
 characters is "bcb", so your function should return 3.
 """
 
-def get_all_substrings(s):
-    substrings = []
-
-    for i in range(len(s)):
-        for j in range(i + 1, len(s) + 1):
-            substrings.append(s[i:j])
-
-    return substrings
-
-def longest_substring_helper(l, k):
-    counts = {}
-
-    for substring in l:
-        characters = set()
-        count = 0
-
-        for character in substring:
-            if character not in characters:
-                characters.add(character)
-                count += 1
-
-        if count <= k:
-            counts[substring] = count
-
+def longest_substring(s, k):
     longest = 0
+    characters = []
+    count = 0
 
-    for substring in counts:
-        if len(substring) > longest:
-            longest = len(substring)
+    for character in s:
+        count += 1
+
+        if character in characters:
+            characters.remove(character)
+
+        characters.append(character)
+
+        if len(characters) > k:
+            count -= 1
+            characters.pop(0)
+
+        if count > longest:
+            longest = count
 
     return longest
-
-def longest_substring(s, k):
-    substrings = get_all_substrings(s)
-
-    return longest_substring_helper(substrings, k)
 
 print(True if longest_substring('aaaaa', 2) == 5 else False)
 print(True if longest_substring('abcba', 2) == 3 else False)
 print(True if longest_substring('abcda', 3) == 3 else False)
 print(True if longest_substring('a', 2) == 1 else False)
 print(True if longest_substring('abcd', 0) == 0 else False)
+print(True if longest_substring('abcaca', 2) == 4 else False)
+print(True if longest_substring('abcde', 4) == 4 else False)
